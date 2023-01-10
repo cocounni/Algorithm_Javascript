fc4da0054c6879e13c556f5c923261184982f7be
cocounni <80098315+cocounni@users.noreply.github.com>
2023-01-10 12:04:11 +0900

    Delete Solution.kt

--- Solution.kt
+++
@@ -1,9 +0,0 @@
-class Solution {
-    fun solution(babbling: Array<String>): Int {
-        val regex = "aya|ye|woo|ma".toRegex()
-        val answer: Int = babbling.map { it.replace(regex, "") }
-            .filter { it.length == 0 }
-            .count()
-        return answer
-    }
-}
